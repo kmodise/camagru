fa0352c01cff51806866b3a916e26b4ce2a5c74c
Keabetswe Justice MODISE <kmodise@c5r6s5.wethinkcode.co.za>
2019-10-25 19:34:01 +0200

    25 oct 19:33

--- table.php
+++ table.php
@@ -1,10 +1,12 @@
 <?php
 //include("connection.php");
-CREATE TABLE IF NOT EXISTS users(
-    userid  INT(10) AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50),
-    fullname VARCHAR(50),
-    email VARCHAR(50),
-    passwd VARCHAR(50))
+// CREATE TABLE IF NOT EXISTS users(
+//     userid  INT(10) AUTO_INCREMENT PRIMARY KEY,
+//     username VARCHAR(50),
+//     fullname VARCHAR(50),
+//     email VARCHAR(50),
+//     passwd VARCHAR(50),
+//     vkey VARCHAR(50),
+//     verified INT(1));
     //$conn->execute();
 ?>
